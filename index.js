8f80811bdfedf0df1ae722aea7fcbfa04aeb61c6
lizwachira <lizwanguwachira@gmail.com>
2024-04-08 18:39:16 +0300

    arraymapmethod

--- index.js
+++ index.js
@@ -13,4 +13,16 @@ const tutorials = [
 
 const titleCased = () => {
   return tutorials
+  const titleCased = () => {
+    return tutorials.map((sentence) => {
+      return sentence
+        .split(" ")
+        .map((word) => {
+          return word.charAt(0).toUpperCase() + word.slice(1);
+        })
+        .join(" ");
+    });
+  }
 }
+//test my output
+console.log("Today is", new Date().toLocaleDateString(), "and the current time is", new Date().toLocaleTimeString());
